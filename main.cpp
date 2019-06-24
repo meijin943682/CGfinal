b110f67a6b440cd4fe5e6a283d0529da7decf54e
meijin943682 <meijin943682@gmail.com>
2019-06-24 12:06:27 +0800

    change output format

--- main.cpp
+++ main.cpp
@@ -15,14 +15,14 @@
 #define TMIN 0.001f
 #define TMAX 10000
 #define TRACEMAX 5
-#define SPECULAR 50
+#define SPECULAR 49
 #define WIDTH 600
 #define HEIGHT 300
+#define TIME 100
 
 using namespace std;
 vector<hitable*> hitable_list;
 vector<pair<vec3, vec3> > light_list;
-uint8_t image[WIDTH][HEIGHT][3][3];
 
 vec3 refract(vec3& I, vec3& N, float eta, bool inner){
   eta = inner? 1 / eta : eta;
@@ -36,7 +36,7 @@ vec3 refract(vec3& I, vec3& N, float eta, bool inner){
   return unit_vector(R);
 }
 
-vec3 shading(hit_record ht, bool inner, vec3 reflect_ray)
+vec3 shading(hit_record ht, bool inner, vec3 reflect_ray, float lightRatio)
 {
   vec3 color(0, 0, 0);
   for(int i = 0; i < int(light_list.size()); i += 1){
@@ -67,8 +67,9 @@ vec3 shading(hit_record ht, bool inner, vec3 reflect_ray)
         }
       }
       if(!flag){
+        lightRatio = lightRatio > 0.5 ? lightRatio : 0;
         color_new = max(float(0), dot(ht.normal * (inner? -1 : 1), shadowRay[i].direction())) * lightIntensity * Kd
-                    + pow(dot(reflect_ray, lightDir), SPECULAR) * lightIntensity;
+                    + max(double(0), pow(dot(reflect_ray, lightDir * -1), SPECULAR)) * lightIntensity * lightRatio;
       }
       else{
         float alpha = (1 - record.w_r) * (1 - record.w_t);
@@ -101,10 +102,11 @@ vec3 trace(vec3 p, vec3 d, int step, bool inner, float lightRatio){
 
   if(record.t != TMAX){
     float length = record.t * r.direction().length();
+    lightRatio -= length / 50;
     vec3 reflect_ray = unit_vector(d - 2 * dot(d, record.normal) * record.normal);
-    local = shading(record, inner, reflect_ray);
-    reflected = trace(record.p, reflect_ray, step + 1, inner, lightRatio - length / 50);
-    transmitted = trace(record.p, refract(d, record.normal, 1.0f / record.material, inner), step + 1, !inner, lightRatio - length / 50);
+    local = shading(record, inner, reflect_ray, lightRatio);
+    reflected = trace(record.p, reflect_ray, step + 1, inner, lightRatio);
+    transmitted = trace(record.p, refract(d, record.normal, 1.0f / record.material, inner), step + 1, !inner, lightRatio);
   }
   else{
     float t = 0.5 * (d.y() + 1.0);
@@ -116,30 +118,26 @@ vec3 trace(vec3 p, vec3 d, int step, bool inner, float lightRatio){
 
 void scene_create(){
   
-  light_list.push_back(make_pair(vec3(0, 0, 0), vec3(1, 1, 1)));
-  light_list.push_back(make_pair(vec3(-10, 0, 0), vec3(1, 0, 0)));
-  light_list.push_back(make_pair(vec3(10, 0, 0), vec3(0, 1, 0)));
+  light_list.push_back(make_pair(vec3(-10, 0, 0), vec3(1, 1, 1)));
+  light_list.push_back(make_pair(vec3(10, 0, 0), vec3(1, 1, 1)));
   
   hitable_list.push_back(new plane(vec3(0, -5, 0), vec3(0, 1, 0), vec3(1.0f, 0.0f, 0.0f))); //ground
-  hitable_list.push_back(new sphere(vec3(0, 0, -2), 0.5, vec3(1.0f, 1.0f, 1.0f), 0.0f, 0.9f, 1.46f));
-  hitable_list.push_back(new sphere(vec3(1, 0, -1.75), 0.5, vec3(1.0f, 1.0f, 1.0f), 0.9f));
-  hitable_list.push_back(new sphere(vec3(-1, 0, -2.25), 0.5, vec3(1.0f, 0.7f, 0.3f)));
-  hitable_list.push_back(new cylinder(vec3(2, 0.5, -2), vec3(0, -1, 0), 0.5, 0.5, vec3(1.0f, 1.0f, 1.0f), 0.9f));
-  hitable_list.push_back(new cylinder(vec3(-2, 0.5, -2), vec3(0, -1, 0), 0.5, 0.5, vec3(1.0f, 1.0f, 1.0f)));
-  hitable_list.push_back(new cone(vec3(3, 0.5, -2), vec3(0, 1, 0), 5, 1, vec3(1.0f, 1.0f, 1.0f), 0.9f));
-  hitable_list.push_back(new cone(vec3(5, 0, -10), vec3(0, -1, 0), 3, 1, vec3(1.0f, 1.0f, 1.0f)));
+  hitable_list.push_back(new compose(
+                         new compose(
+                         new compose(
+                         new compose(new cube(vec3(-2, -2, -2), vec3(4, 0, 0), vec3(0, 4, 0), 4, vec3(1.0f, 1.0f, 0.0f)),
+                                     new cube(vec3(-1.5, -1.5, -1), vec3(3, 0, 0), vec3(0, 3, 0), 6, vec3(1.0f, 0.0f, 0.0f)), 1),
+                                     new cube(vec3(-3, -1.5, -2.5), vec3(6, 0, 0), vec3(0, 3, 0), 3, vec3(0.0f, 0.0f, 1.0f)), 1),
+                                     new cube(vec3(-1.5, -3, -2.5), vec3(3, 0, 0), vec3(0, 6, 0), 3, vec3(0.0f, 1.0f, 0.0f)), 1),
+                                     new sphere(vec3(0, 0, -4), 1.5, vec3(1, 1, 1), 0.0f, 0.9f, 1.46f), 0)); 
+
+  hitable_list.push_back(new compose(new sphere(vec3(4, 0, -4), 1.5, vec3(1, 1, 1), 0.9f), 
+                                     new sphere(vec3(5, 0, -4), 1.5, vec3(1, 1, 1), 0.9f), 2));
   
-  hitable_list.push_back(new compose(new sphere(vec3(0, 1.5, -10), 3, vec3(1.0f, 1.0f, 1.0f), 0.9f), 
-                                     new compose(new sphere(vec3(-1.5, 0, -10), 3, vec3(1.0f, 1.0f, 1.0f), 0.9f), 
-                                                 new sphere(vec3(1.5, 0, -10), 3, vec3(1.0f, 1.0f, 1.0f), 0.9f), 0), 0));
-  
-  hitable_list.push_back(new compose(new sphere(vec3(2, 0, -10), 3, vec3(1.0f, 1.0f, 1.0f), 0.0f, 0.9f), 
-                                     new sphere(vec3(2, 0, -10), 2, vec3(1.0f, 1.0f, 1.0f)), 1));
-  
-  hitable_list.push_back(new compose(new sphere(vec3(0, -1, -4), 1.5, vec3(1, 1, 1), 0.0f, 0.9f, 1.46f),
-                         new compose(new cube(vec3(-2, -3, -2), vec3(4, 0, 0), vec3(0, 4, 0), 4, vec3(1.0f, 1.0f, 0.0f)),
-                                     new cube(vec3(-1.8, -2.8, -1), vec3(3.6, 0, 0), vec3(0, 3.6, 0), 6, vec3(1.0f, 0.0f, 0.0f)), 1), 2));
- srand(1234);
+  hitable_list.push_back(new compose(new sphere(vec3(-4, 0, -4), 1.5, vec3(1, 0, 0)), 
+                                     new sphere(vec3(-5, 0, -4), 1.5, vec3(0, 1, 0)), 0));
+ /*
+  srand(1234);
 
   vec3 colorlist[8] = { vec3(0.8, 0.3, 0.3), vec3(0.3, 0.8, 0.3), vec3(0.3, 0.3, 0.8),
     vec3(0.8, 0.8, 0.3), vec3(0.3, 0.8, 0.8), vec3(0.8, 0.3, 0.8),
@@ -153,45 +151,53 @@ void scene_create(){
     rand_reflec = rand_reflec > 0.5 ? rand_reflec : 0;
     hitable_list.push_back(new sphere(vec3(xr, yr, -15), 3, colorlist[cindex], rand_reflec));
   }
-
+  */
 }
-
 
 int main()
 {
-  fstream file;
-  file.open("ray2.ppm", ios::out);
-
-  vec3 lower_left_corner(-2, -1, -1);
   vec3 origin(0, 0, 1);
   vec3 horizontal(4, 0, 0);
   vec3 vertical(0, 2, 0);
-  camera cam(vec3(2, 0, 10), vec3(0, 0, -1), vec3(0, 1, 0), 60,  float(WIDTH) / float(HEIGHT), 0.1, 11);
-
-  file << "P3\n" << WIDTH << " " << HEIGHT << "\n255\n";
-  
+  vec3 point(0, 3, 10);
+  camera cam(point, vec3(0, 0, -1), vec3(0, 1, 0), 60,  float(WIDTH) / float(HEIGHT), 0.1, 11);
   scene_create();
+  
+  int interval = TIME;
+  bool isMoving = false;
 
-  for (int j = HEIGHT - 1; j >= 0; j--) {
-    for (int i = 0; i < WIDTH; i++) {
-      float u[2], v[2];
-      u[0] = float(i) / float(WIDTH);
-      v[0] = float(j) / float(HEIGHT);
-      u[1] = (float(i) + 0.5) / float(WIDTH);
-      v[1] = (float(j) - 0.5) / float(HEIGHT);
+  for(int i = 0; i < TIME; i += interval){
+    fstream file;
+    file.open("output/ray" + to_string(i) + ".ppm", ios::out);
+    file << "P3\n" << WIDTH << " " << HEIGHT << "\n255\n";
+    
+    for (int j = HEIGHT - 1; j >= 0; j--) {
+      for (int i = 0; i < WIDTH; i++) {
+        float u[2], v[2];
+        u[0] = float(i) / float(WIDTH);
+        v[0] = float(j) / float(HEIGHT);
+        u[1] = (float(i) + 0.5) / float(WIDTH);
+        v[1] = (float(j) - 0.5) / float(HEIGHT);
 
-      vec3 color_mix(0, 0, 0);
-      for(int h = 0; h < 2; h++){
-        for(int k = 0; k < 2; k++){
-          ray r = cam.get_ray(u[h], v[k]);
-          color_mix = color_mix + trace(r.origin(), unit_vector(r.direction()), 0, false, 1.0);
+        vec3 color_mix(0, 0, 0);
+        for(int h = 0; h < 2; h++){
+          for(int k = 0; k < 2; k++){
+            ray r = cam.get_ray(u[h], v[k]);
+            color_mix = color_mix + trace(r.origin(), unit_vector(r.direction()), 0, false, 1.0);
+          }
         }
+        vec3 mean_color = color_mix / 4;
+        mean_color = vec3(min(1.0f, mean_color.x()), min(1.0f, mean_color.y()), min(1.0f, mean_color.z()));
+        file << int(mean_color.r() * 255) << " " << int(mean_color.g()  * 255) << " " << int(mean_color.b() * 255) << "\n";
       }
-      vec3 mean_color = color_mix / 4;
-      mean_color = vec3(min(1.0f, mean_color.x()), min(1.0f, mean_color.y()), min(1.0f, mean_color.z()));
-      file << int(mean_color.r() * 255) << " " << int(mean_color.g()  * 255) << " " << int(mean_color.b() * 255) << "\n";
+    }
+    
+    file.close();
+    if(isMoving == true){
+      int dir = (i > 25 && i <= 75) ? 1 : -1;
+      for(int i = 1; i < int(hitable_list.size()); i++)
+        hitable_list[i] -> move(vec3(dir, 0, 0), 0.4 * interval);
     }
   }
-
   return 0;
 }
